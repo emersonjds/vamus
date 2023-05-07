371176e2530ead751888f29e9f2b8fb75fe79f24
Emerson Silva <emerson_jdss@hotmail.com>
2023-05-07 05:37:27 -0300

    create a initial card for notifications

--- src/screens/Notification/index.tsx
+++ src/screens/Notification/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import { Box } from '../../components/Spacing';
 import { Divider, Text } from 'react-native-paper';
-import { ScreenContainer } from 'react-native-screens';
+
 import { Container } from '../../components/Spacing';
-import Timeline from 'react-native-timeline-flatlist';
 
 
 const Notification: React.FC = () => {
@@ -12,68 +11,60 @@ const Notification: React.FC = () => {
   return (
     <>
       <Container>
-
         <Text
           variant="titleLarge"
         >Notificações</Text>
         <Text>
           Aqui voce acompanha o andamento das suas viagens, interesses, trips fechadas e desistencias
         </Text>
-
         <Box
           mt={10}
         >
           <Divider />
         </Box>
-
         <Box
           width="100%"
-          height={52}
           bg="red"
           flexDirection="row"
+          justifyContent="space-between"
+          borderRadius={5}
+          mt={10}
+          borderWidth={1}
+          padding={2}
         >
           <Box
             width="20%"
             height={52}
           >
-            <Text>
-              Imagem
-            </Text>
+            <Image
+              style={{ width: '100%', height: '100%' }}
+              source={require('../../assets/logo/logo-copa.jpeg')}
+              resizeMode='contain'
+            />
           </Box>
-
           <Box
             width="75%"
             height={52}
           >
-
             <Box
               width="100%"
               flexDirection="row"
               justifyContent="space-between"
             >
               <Text>
-                Nome da Trip
+                Viagem - Spectrum - Espanha
               </Text>
               <Text>
-                Data da Trip
+                23/04
               </Text>
             </Box>
-
             <Box>
               <Text>
-                lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+                A empresa Copastur esta analisando essa viagem e logo entrara em contato
               </Text>
             </Box>
-
-
           </Box>
-
         </Box>
-
-
-
-
-
       </Container>
     </>
   );
