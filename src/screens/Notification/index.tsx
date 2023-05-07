114aa0834834da7a2da96ab14359f34ec460225c
Emerson Silva <emerson_jdss@hotmail.com>
2023-05-07 05:32:05 -0300

    adjust Notification Area

--- src/screens/Notification/index.tsx
+++ src/screens/Notification/index.tsx
@@ -1,26 +1,103 @@
 import React from 'react';
-import { View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { Box } from '../../components/Spacing';
-import { Text, Title } from 'react-native-paper';
+import { Divider, Text } from 'react-native-paper';
 import { ScreenContainer } from 'react-native-screens';
 import { Container } from '../../components/Spacing';
+import Timeline from 'react-native-timeline-flatlist';
 
-// import { Container } from './styles';
 
 const Notification: React.FC = () => {
-    return (
-        <>
-            <Container>
 
-                <Title>Notificações</Title>
-                <Text>
-                    Emerson
-                </Text>
+  return (
+    <>
+      <Container>
+
+        <Text
+          variant="titleLarge"
+        >Notificações</Text>
+        <Text>
+          Aqui voce acompanha o andamento das suas viagens, interesses, trips fechadas e desistencias
+        </Text>
+
+        <Box
+          mt={10}
+        >
+          <Divider />
+        </Box>
+
+        <Box
+          width="100%"
+          height={52}
+          bg="red"
+          flexDirection="row"
+        >
+          <Box
+            width="20%"
+            height={52}
+          >
+            <Text>
+              Imagem
+            </Text>
+          </Box>
+
+          <Box
+            width="75%"
+            height={52}
+          >
+
+            <Box
+              width="100%"
+              flexDirection="row"
+              justifyContent="space-between"
+            >
+              <Text>
+                Nome da Trip
+              </Text>
+              <Text>
+                Data da Trip
+              </Text>
+            </Box>
+
+            <Box>
+              <Text>
+                lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget
+              </Text>
+            </Box>
 
 
-            </Container>
-        </>
-    );
+          </Box>
+
+        </Box>
+
+
+
+
+
+      </Container>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginBottom: 16
+  },
+  timeText: {
+    width: 80,
+    marginRight: 16,
+    color: 'gray'
+  },
+  descriptionText: {
+    flex: 1,
+    color: 'black'
+  }
+});
 
 export default Notification;
