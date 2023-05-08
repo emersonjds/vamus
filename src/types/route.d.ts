e49053f7c02e4448175f30dc5d7de349d69c7052
Emerson Silva <emerson_jdss@hotmail.com>
2023-05-08 10:35:55 -0300

    add route types

---
+++ src/types/route.d.ts
@@ -0,0 +1,11 @@
+export type RootStackParamList = {
+  DetailsTrip: {
+    uri?: string;
+    title?: string;
+    views?: number;
+    pp?: string;
+    onPress?: () => void;
+    icon?: any;
+    country?: string;
+  }
+};
