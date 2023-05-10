0fcef77b175808a9de58bb906223ed0d53cbe202
Emerson Silva <emerson_jdss@hotmail.com>
2023-05-09 22:40:44 -0300

    adjust style on Box

--- src/components/Heart/index.tsx
+++ src/components/Heart/index.tsx
@@ -5,8 +5,7 @@ import { AntDesign } from '@expo/vector-icons';
 const Heart: React.FC = () => {
   return (
     <>
-      <Box
-      >
+      <Box>
         <AntDesign name="hearto" size={24} color="red" />
       </Box>
     </>
