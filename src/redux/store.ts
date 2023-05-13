fadc4b8856b36086878118be5133e7895163a601
Emerson Silva <emerson_jdss@hotmail.com>
2023-05-13 16:31:02 -0300

    adjust error on store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import tripReducer from '../redux/slices/tripSlice';
+
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        trip: tripReducer,
+    },
 });
 
 export default store;
+
